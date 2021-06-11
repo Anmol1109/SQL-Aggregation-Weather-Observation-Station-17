e6b800be383ea2baac5901235cac12ed167725b7
ANMOL PANT <48082902+Anmol1109@users.noreply.github.com>
2021-06-11 23:26:01 +0530

    Update Solution.sql

--- Solution.sql
+++ Solution.sql
@@ -2,4 +2,8 @@
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
-select round(LONG_W,4) from station where LAT_N = (select min(LAT_N) from station where LAT_N > 38.7780);
+select round(LONG_W,4) 
+from station 
+where LAT_N = (select min(LAT_N) 
+               from station 
+               where LAT_N > 38.7780);
